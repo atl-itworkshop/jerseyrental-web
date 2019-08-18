9d371d9cd715c1c674f2a3645ae38e35df841eb7
Milton Chandradas <milton.chandradas@sap.com>
2019-08-18 17:29:32 -0400

    Comment here…

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,23 +1,51 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
+const Header = ({ title }) => {
+   return (
+      <nav className="navbar bg-primary">
+         <h1>{title}</h1>
+         <ul>
+            <li>
+               <Link to="/">Home</Link>
+            </li>
+            <li>
+               <Link to="/">Rent</Link>
+            </li>
+            <li>
+               <Link to="/">Events</Link>
+            </li>
+            <li>
+               <Link to="/">Help</Link>
+            </li>
+            <li>
+               <Link to="/">How it works</Link>
+            </li>
+            <li>
+               <Link to="/">
+                  <i className="fas fa-search" />
+               </Link>
+            </li>
+            <li>
+               <Link to="/">
+                  <button className="btn-med">Sign in / up</button>
+               </Link>
+            </li>
+            <li>
+               <Link to="/about">About</Link>
+            </li>
+         </ul>
+      </nav>
+   );
+};
 
-function Header() {
-  return (
-    <div>
-      <header style={headerStyle}>
-        <h1>Jersey Rental</h1>
-        
-      </header>
-    </div>
-  );
-}
+Header.defaultProps = {
+   title: "Jersey Rental"
+};
 
-const headerStyle = {
-
-    background: "#333",
-    color: "#fff",
-    textAlign: "center",
-    padding: "10px"
-}
+Header.propTypes = {
+   title: PropTypes.string.isRequired
+};
 
 export default Header;
---
+++ src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+
+            <h1>About</h1>
+            <p>This is the Jersey Rental app v1.0.0.</p>
+            <p>Sports Fans Unite !!.  </p>
+            <p>Never go to a sporting event without your favorite jersey...</p>
+            
+        </React.Fragment>
+    )
+}
+
+
+export default About;
